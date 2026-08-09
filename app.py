0d1288b362af4b637c719d99d0e6057a01882bd3
humayun attar <irfanattar268@gmail.com>
2026-08-10 02:47:24 +0530

    Fix LangChain imports and requirements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from pathlib import Path
 from dotenv import load_dotenv
 
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import (
